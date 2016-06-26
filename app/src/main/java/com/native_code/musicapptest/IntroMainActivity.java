b94d5b3627e945e04e014e62af470974067d1a23
Anthony <anthonios10@gmail.com>
2016-06-26 19:19:37 +0930

    Permissions intro slide is only shown on Android 6 and above

--- app/src/main/java/com/native_code/musicapptest/IntroMainActivity.java
+++ app/src/main/java/com/native_code/musicapptest/IntroMainActivity.java
@@ -2,14 +2,12 @@ package com.native_code.musicapptest;
 
 import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-/**
- * Created by Antho on 24/06/2016.
- */
 public class IntroMainActivity extends IntroActivity {
 
     @Override
@@ -29,17 +27,22 @@ public class IntroMainActivity extends IntroActivity {
                 .scrollable(true)
                 .build());
 
-        // add the second slide
-        addSlide(new SimpleSlide.Builder()
-                .title(R.string.intro_slide_two_title)
-                .description(R.string.intro_slide_two_desc)
-                .background(R.color.intro_page_two)
-                .backgroundDark(R.color.intro_page_two_dark)
-                .permission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                .image(R.drawable.ic_library_music)
-                .scrollable(true)
-                .build());
+        // check if device is running Android 6 or above, if so, ask for permissions
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= Build.VERSION_CODES.M) {
 
+            // add the second slide for permissions
+            addSlide(new SimpleSlide.Builder()
+                    .title(R.string.intro_slide_two_title)
+                    .description(R.string.intro_slide_two_desc)
+                    .background(R.color.intro_page_two)
+                    .backgroundDark(R.color.intro_page_two_dark)
+                    .permission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .image(R.drawable.ic_library_music)
+                    .scrollable(true)
+                    .build());
+
+        }
     }
 
 }
